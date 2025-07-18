bbefcc62f311d0369935d3e1fa9f771c12f6443f
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-07-18 08:37:54 +0700

    add: setup auto migration database

---
+++ database/migratios/migratios.go
@@ -0,0 +1,18 @@
+package migratios
+
+import (
+	"booking-system/internal/backend/domain/entity"
+	"log"
+
+	"gorm.io/gorm"
+)
+
+func Migrate(db *gorm.DB) error {
+	err := db.AutoMigrate(&entity.User{}, &entity.Bucket{})
+	if err != nil {
+		return err
+	}
+	log.Println("Database migration completed successfully")
+	return nil
+
+}
