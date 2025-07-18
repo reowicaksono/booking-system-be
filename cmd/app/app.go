635d888db705e38edbade91e9035e25d4b6b3750
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-07-18 08:36:43 +0700

    add: setup main project

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,1 +1,39 @@
 package app
+
+import (
+	"booking-system/database/migratios"
+	"booking-system/internal/backend/infrastructure/config"
+	"booking-system/internal/pkg/drivers"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type App struct {
+	DB     *gorm.DB
+	Router *gin.Engine
+}
+
+// DB initializes the database connection
+func (app *App) DBInit() {
+	mysqConn := drivers.MysqlConnection()
+	app.DB = mysqConn
+
+	err := migratios.Migrate(app.DB)
+
+	if err != nil {
+		panic("Failed to migrate database: " + err.Error())
+	}
+}
+
+func (app *App) RouterInit() {
+	router := gin.Default()
+
+	app.Router = router
+}
+
+func (app *App) Run() {
+	port := fmt.Sprintf(":%s", config.GetEnv("APP_PORT", "8080"))
+	app.Router.Run(port)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,12 @@
 package main
+
+import "booking-system/cmd/app"
+
+func main() {
+	// Initialize the application
+	app := app.App{}
+
+	app.DBInit()
+	app.RouterInit()
+	app.Run()
+}
