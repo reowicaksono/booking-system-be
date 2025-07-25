24d0de5c0ad4ae80783b754785fc4c37b014395a
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-07-25 08:31:17 +0700

    add: user profile & admin user

--- internal/backend/domain/entity/user.go
+++ internal/backend/domain/entity/user.go
@@ -7,25 +7,53 @@ import (
 )
 
 type User struct {
-	ID          uint   `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name        string `gorm:"type:varchar(50)" json:"name"`
-	Username    string `gorm:"type:varchar(50);unique;not null" json:"username"`
-	Email       string `gorm:"type:varchar(50);unique" json:"email"`
-	PhoneNumber string `gorm:"type:varchar(15)" json:"phone_number"`
-	Password    string `gorm:"type:varchar(128)" json:"-"`
-	Address     string `gorm:"type:varchar(200)" json:"address"`
-	IsActive    bool   `gorm:"type:boolean;default:false" json:"is_active"`
-	Role        string `gorm:"type:varchar(10)" json:"role"`
-	WebToken    string `gorm:"type:text" json:"-"`
-	DeviceToken string `gorm:"type:text" json:"-"`
+	ID          uint           `gorm:"primaryKey;autoIncrement" json:"id"`
+	Username    string         `gorm:"type:varchar(50);unique" json:"username"`
+	Name        string         `gorm:"type:varchar(50)" json:"name"`
+	Email       string         `gorm:"type:varchar(50);unique" json:"email"`
+	PhoneNumber string         `gorm:"type:varchar(15)" json:"phone_number"`
+	Password    string         `gorm:"type:varchar(255);default:null"`
+	Role        string         `gorm:"type:varchar(10)" json:"role"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 
-	Bucket *Bucket `gorm:"foreignKey:UserId;references:ID;onDelete:RESTRICT;onUpdate:CASCADE" json:"-"`
-
-	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	Bucket    *Bucket    `gorm:"foreignKey:UserId;references:ID;onDelete:RESTRICT;onUpdate:CASCADE" json:"-"`
+	Profile   *Profile   `gorm:"foreignKey:UserId;references:ID;onDelete:RESTRICT;onUpdate:CASCADE" json:"-"`
+	AdminUser *AdminUser `gorm:"foreignKey:UserId;references:ID;onDelete:RESTRICT;onUpdate:CASCADE" json:"-"`
 }
 
 func (u User) isRole(role string) bool {
 	return u.Role == role
+}
+
+type Profile struct {
+	ID          uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	UserId      uint   `gorm:"not null" json:"user_id"`
+	AdminId     uint   `gorm:"not null" json:"admin_id"`
+	Name        string `gorm:"type:varchar(50)" json:"name"`
+	PhoneNumber string `gorm:"type:varchar(15)" json:"phone_number"`
+	Address     string `gorm:"type:varchar(255)" json:"address"`
+
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
+
+type AdminUser struct {
+	UserId  uint `gorm:"not null" json:"user_id"`
+	AdminId uint `gorm:"not null" json:"admin_id"`
+
+	Admin *Admin `gorm:"foreignKey:AdminId;references:ID;onDelete:RESTRICT;onUpdate:CASCADE" json:"-"`
+}
+
+func (u User) TableName() string {
+	return "users"
+}
+
+func (p Profile) TableName() string {
+	return "user_profiles"
+}
+
+func (au AdminUser) TableName() string {
+	return "admin_users"
 }
